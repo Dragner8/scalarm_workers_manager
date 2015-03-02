37e1e09402a835b21795697c5eada019d77dbe45
mwrona <michal.wrona.4@gmail.com>
2015-03-02 14:35:47 +0100

    Workers manager -> monitoring

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf $PACKAGES_DIR/*
 
 for OS in linux; do
     for ARCH in amd64 386; do
-        BIN_PATH="$PACKAGES_DIR/${OS}_${ARCH}/scalarm_workers_manager"
+        BIN_PATH="$PACKAGES_DIR/${OS}_${ARCH}/scalarm_monitoring"
         echo "Building: $OS $ARCH in ${BIN_PATH}..."
         GOOS=$OS GOARCH=$ARCH CGO_ENABLED=0 go build -o $BIN_PATH
         strip $BIN_PATH
